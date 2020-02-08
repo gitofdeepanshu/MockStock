dd1411c460aa0b2f44b98a263f095f7dd77307a7
Ashutosh Varma <ashutoshvarma11@live.com>
2020-02-08 13:56:07 +0530

    add template for main.index

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,15 +3,14 @@ from flask import render_template, flash, redirect, url_for, request, g, \
     jsonify, current_app
 from flask_login import current_user, login_required
 from app import db
-from app.models import User
+from app.models import User, Stock
 from app.main import bp
 
 
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
-@login_required
+# @login_required
 def index():
-    return "Logged In"
-
+    return render_template('main/index.html', stocks=Stock.query.all())
 
